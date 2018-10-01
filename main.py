c9b4df70c84de29c6f9625c312095661b23eb9a9
zehnpaard <zehnpaard@googlemail.com>
2018-10-01 10:39:16 +0900

    Implement initializer for Function class

--- main.py
+++ main.py
@@ -69,3 +69,19 @@ def Function:
             '__name__', '__dict__', '__doc__',
             '_vm', '_func',]
 
+    def __init__(self, name, code, globs, defaults, closure, vm):
+        self._vm = vm
+        self.func_code = code
+        self.func_name = self.__name__ = name or code.co_name
+        self.func_defaults = tuple(defaults)
+        self.func_globals = globs
+        self.func_locals = self._vm.frame.f_locals
+        self.__dict__ = {}
+        self.func_closure = closure
+        self.__doc__ = code.co_consts[0] if code.co_consts else None
+
+        kw = {'argdefs': self.func_defaults}
+        if closure:
+            kw['closure'] = tuple(make_cell(0) for _ in closure)
+        self._func = types.FunctionType(code, globs, **kw)
+
